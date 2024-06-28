5eb4177d7bec8a9ea12242fdaba40e2da2e6f8b3
Aqua Dolf <aaqddf@icloud.com>
2024-06-28 20:42:36 +1000

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import {Analytics} from '@vercel/analytics/react'
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./wc.css"
@@ -37,6 +38,7 @@ export default function RootLayout({
         >
           <span className="p-2 text-[var(--yellow-11)]">farp</span>
         </footer>
+        <Analytics/>
         </body>
     </html>
   );
