59998d50fd21c24f761d5c9ee94ad1107641ee5b
eucalyptus-viminalis <65995595+eucalyptus-viminalis@users.noreply.github.com>
2025-03-30 12:55:20 +0900

    fix layout + context state

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,19 +2,11 @@
 
 import sdk, { Context } from "@farcaster/frame-sdk";
 import dynamic from "next/dynamic";
-import { ReactNode, useEffect, useState } from "react";
-import { DCEditProvider } from "./_context/DCEditContext";
-import { EditProvider } from "./_context/EditContext";
-import { FarpletProvider } from "./_context/FarpletContext";
-import { GlobalContextProvider } from "./_context/GlobalContext";
-import MyQueryClientProvider from "./_context/QueryContext";
+import { useEffect, useState } from "react";
 
-const WagmiBaseProvider = dynamic(
-  () => import("@/app/_context/WagmiProvider"),
-  {
-    ssr: false,
-  },
-);
+const WagmiBaseProvider = dynamic(() => import("@/app/_context/WagmiProvider"), {
+  ssr: false,
+});
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [context, setContext] = useState<Context.FrameContext | undefined>();
@@ -35,24 +27,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
       load();
     }
   }, [isSDKLoaded]);
-
-  const InnerProviders = ({ children }: { children: ReactNode }) => (
-    <MyQueryClientProvider>
-      <GlobalContextProvider>
-        <EditProvider>
-          <DCEditProvider>
-            <FarpletProvider>{children}</FarpletProvider>
-          </DCEditProvider>
-        </EditProvider>
-      </GlobalContextProvider>
-    </MyQueryClientProvider>
-  );
-
-  return context ? (
-    <WagmiBaseProvider>
-      <InnerProviders>{children}</InnerProviders>
-    </WagmiBaseProvider>
-  ) : (
-    <InnerProviders>{children}</InnerProviders>
+  // only add provider if context loaded?
+  return (
+    <>
+      {context ? <WagmiBaseProvider>{children}</WagmiBaseProvider> : children}
+    </>
   );
 }
