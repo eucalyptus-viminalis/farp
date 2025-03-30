393179647ed870062d9c67fbf8e06304e59101c1
Aqua Dolf <fttx5525@gmail.com>
2025-03-30 12:55:20 +0900

    fix layout + context state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,15 @@ import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import "./wc.css";
 import "../css/custom.css";
-import PageButtons from "./_ui/PageButtons";
 import { inter } from "@/fonts/fonts";
 import { Providers } from "./providers";
 import { ColorSchemeProvider } from "./_context/ColorSchemeCtx";
+import MyQueryClientProvider from "./_context/QueryContext";
+import { GlobalContextProvider } from "./_context/GlobalContext";
+import { EditProvider } from "./_context/EditContext";
+import { DCEditProvider } from "./_context/DCEditContext";
+import { FarpletProvider } from "./_context/FarpletContext";
+import PageButtons from "./_ui/PageButtons";
 import ModeButtons from "./_ui/ModeButtons";
 
 export const metadata: Metadata = {
@@ -30,46 +35,56 @@ export default function RootLayout({
       <ColorSchemeProvider>
         <body className={inter.className + " overflow bg-app w-screen"}>
           <Providers>
-            <div className="container mx-auto min-h-full h-max">
-              <div className="flex min-h-screen flex-row justify-center">
-                <main className="h-full bg-app relative w-full overflow-x-hidden shrink-0 justify-center sm:w-[540px] lg:w-[620px]">
-                  {/* Bleed */}
-                  <div
-                    className="
-                      absolute top-0 -translate-x-full h-full left-0 w-full
-                      z-10
-                      bg-gradient-to-l
-                      from-app-tw-light dark:from-app-tw-dark
-                      from-20%
-                      to-[var(--yellow-6)] dark:to-[var(--yellow-6)]
-                    "
-                  ></div>
-                  <div
-                    className="
-                        absolute top-0 translate-x-full h-full right-0 w-full
-                        z-10
-                        bg-gradient-to-r from-app-tw-light dark:from-app-tw-dark
-                        from-20%
-                        to-[var(--yellow-6)] dark:to-[var(--yellow-6)]
-                    "
-                  ></div>
-                  <div className="w-full h-full">
-                    <div className="h-full min-h-screen border-default sm:border-x">
-                      <span className="absolute text-[var(--yellow-8)]">
-                        [beta]
-                      </span>
-                      <PageButtons />
-                      <ModeButtons />
-                      {children}
-                    </div>
-                  </div>
-                </main>
-              </div>
-            </div>
+            <MyQueryClientProvider>
+              <GlobalContextProvider>
+                <EditProvider>
+                  <DCEditProvider>
+                    <FarpletProvider>
+                      <div className="container mx-auto min-h-full h-max">
+                        <div className="flex min-h-screen flex-row justify-center">
+                          <main className="h-full bg-app relative w-full overflow-x-hidden shrink-0 justify-center sm:w-[540px] lg:w-[620px]">
+                            {/* Bleed */}
+                            <div
+                              className="
+                                              absolute top-0 -translate-x-full h-full left-0 w-full
+                                              z-10
+                                              bg-gradient-to-l
+                                              from-app-tw-light dark:from-app-tw-dark
+                                              from-20%
+                                              to-[var(--yellow-6)] dark:to-[var(--yellow-6)]
+                                            "
+                            ></div>
+                            <div
+                              className="
+                                                absolute top-0 translate-x-full h-full right-0 w-full
+                                                z-10
+                                                bg-gradient-to-r from-app-tw-light dark:from-app-tw-dark
+                                                from-20%
+                                                to-[var(--yellow-6)] dark:to-[var(--yellow-6)]
+                                            "
+                            ></div>
+                            <div className="w-full h-full">
+                              <div className="h-full min-h-screen border-default sm:border-x">
+                                <span className="absolute text-[var(--yellow-8)]">
+                                  [beta]
+                                </span>
+                                <PageButtons />
+                                <ModeButtons />
+                                {children}
+                              </div>
+                            </div>
+                          </main>
+                        </div>
+                      </div>
+                    </FarpletProvider>
+                  </DCEditProvider>
+                </EditProvider>
+              </GlobalContextProvider>
+            </MyQueryClientProvider>
           </Providers>
           {/* <footer className="bg-app border-t py-4 border-faint flex flex-col justify-center items-center">
-            <span className="p-2 text-[var(--yellow-11)]">farp</span>
-          </footer> */}
+                    <span className="p-2 text-[var(--yellow-11)]">farp</span>
+                </footer> */}
           <Analytics />
         </body>
       </ColorSchemeProvider>
