d4303f228b9e87e13b4a0d9d423e722fc6ad1ead
Aqua Dolf <fttx5525@gmail.com>
2025-03-31 16:55:45 +0900

    lay em up hehe

--- src/app/dc/layout.tsx
+++ src/app/dc/layout.tsx
@@ -1,24 +1,42 @@
+import { FrameEmbedNext } from "@farcaster/frame-sdk";
 import { Metadata } from "next";
+import { appConfig } from "../appConfig";
+
+const frame: FrameEmbedNext = {
+  version: "next", // version of the frame protocol this route is using
+  imageUrl: `${appConfig.hostUrl}/dc/opengraph-image.png`, // the image to be displayed before user launches the frame
+  aspectRatio: "3:2", // aspect ratio for this image to be displayed in
+  button: {
+    // details about the button the user will press to launch the mini-app
+    title: "farp", // the text displayed on the button
+    action: {
+      type: "launch_frame", //
+      name: "farp", // name of the app
+      url: appConfig.hostUrl + "/dc", // opens this url in the in-app browser
+      splashImageUrl: `${appConfig.hostUrl}/icon`,
+      splashBackgroundColor: "#ffffff",
+    },
+  },
+};
 
 export const metadata: Metadata = {
-    title: "farp / dc",
+  title: "farp / dc",
+  description: "chat, is this real?",
+  openGraph: {
+    title: "farp",
     description: "chat, is this real?",
-    openGraph: {
-        title: 'farp',
-        description: "chat, is this real?",
-        type: "website",
-    },
-    keywords: ['Farp', 'Farcaster', 'Fake Cast Generator', 'Warpcast'],
-    // metadataBase: 
+    type: "website",
+  },
+  keywords: ["Farp", "Farcaster", "Fake Cast Generator", "Warpcast"],
+  other: {
+    "fc:frame": JSON.stringify(frame),
+  },
 };
+
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <>
-            {children}
-        </>
-    )
+  return <>{children}</>;
 }
--- src/app/farplet/layout.tsx
+++ src/app/farplet/layout.tsx
@@ -1,4 +1,23 @@
+import { FrameEmbedNext } from "@farcaster/frame-sdk";
 import { Metadata } from "next";
+import { appConfig } from "../appConfig";
+
+const frame: FrameEmbedNext = {
+  version: "next", // version of the frame protocol this route is using
+  imageUrl: `${appConfig.hostUrl}/farplet/opengraph-image.png`, // the image to be displayed before user launches the frame
+  aspectRatio: "3:2", // aspect ratio for this image to be displayed in
+  button: {
+    // details about the button the user will press to launch the mini-app
+    title: "farp", // the text displayed on the button
+    action: {
+      type: "launch_frame", //
+      name: "farp", // name of the app
+      url: appConfig.hostUrl + "/farplet", // opens this url in the in-app browser
+      splashImageUrl: `${appConfig.hostUrl}/icon`,
+      splashBackgroundColor: "#ffffff",
+    },
+  },
+};
 
 export const metadata: Metadata = {
   title: "farp / farplet",
@@ -16,8 +35,11 @@ export const metadata: Metadata = {
     "Warplet",
     "Farplet",
   ],
-  // metadataBase:
+  other: {
+    "fc:frame": JSON.stringify(frame),
+  },
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
