ae6f66ccf6f7367dd5fb81f9a014b7780f7746f3
Aqua Dolf <fttx5525@gmail.com>
2025-04-01 18:49:48 +0900

    try

--- src/app/farplet/page.tsx
+++ src/app/farplet/page.tsx
@@ -1,22 +1,14 @@
 "use client";
 // import '@/css/dc.css'
 
-import sdk, { Context } from "@farcaster/frame-sdk";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import FarpletEditNode from "./_ui/FarpletEditNode";
 import FarpletPreviewNode from "./_ui/FarpletPreviewNode";
 import { GlobalContext } from "../_context/GlobalContext";
-import { useFarcasterCtx } from "../_context/FarcasterCtx";
 
 export default function DCPage() {
   const cx = useContext(GlobalContext);
-  const { state: fcState } = useFarcasterCtx();
   const { mode } = cx;
-  useEffect(() => {
-    if (fcState.isSdkLoaded && !fcState.farcasterContext?.client.added) {
-      sdk.actions.addFrame();
-    }
-  }, [fcState]);
   return (
     <>
       {mode === "edit" && (
